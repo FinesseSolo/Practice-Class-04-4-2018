5676a76e1228ade7f35eb197ca83b634e6bb4476
Hancock <bchancock.2017@mymail.jcsu.edu>
2018-04-04 15:13:06 -0400

    first commit

---
+++ Back.java
@@ -0,0 +1,13 @@
+
+public class Back {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String [] names = {"joe","Jack","Jake"};
+for (int i = 2; i >= 0; i --){
+	System.out.println(names[i]);
+	
+}
+	}
+
+}
---
+++ largest.java
@@ -0,0 +1,15 @@
+
+public class largest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] r= {7,18,9,2,81,16};int t = r[0];
+		for (int i = 0; i < r.length; i++) {
+			if(r[i]> t){
+				t=r[i];
+				
+	}
+
+}System.out.println(t);
+}
+}
